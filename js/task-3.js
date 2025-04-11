c25c941d2874b608b0a0934529a3831d54980576
Maksym Movchan <ms.movchan97@gmail.com>
2025-04-11 12:21:47 +0300

    full

--- js/task-3.js
+++ js/task-3.js
@@ -6,3 +6,6 @@ const borderWidth = parseFloat(border);
 return contentWidth + 2 * paddingWidth + 2 * borderWidth;
 }
 
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
